0a7c9363fc2bd03cf840c72072f8f950470c51c5
odiegoduarte <dduartesouza17@gmail.com>
2020-06-27 22:53:56 -0300

    teste

--- postintall_teste.sh
+++ postintall_teste.sh
@@ -5,25 +5,45 @@
 # Informações
 #
 
-#------------------------------Variáveis PPA------------------------------#
+#------------------------------------Variáveis PPA---------------------------------------#
+
 PPA_PULSE_EFFECTS"ppa:mikhailnov/pulseeffects"
 
+#------------------------------------Variáveis.deb---------------------------------------#
 
-#------------------------------Variáveis.deb------------------------------#
 URL_CHROME="https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb"
 
+#---------------------------------Variáveis AppImage-------------------------------------#
 
-#------------------------------Variáveis de pasta------------------------------#
+URL_EMAGE"https://github.com/douglasjunior/emage/releases/download/1.1.0/emage-1.1.0-x86_64.AppImage"
+
+#-----------------------------------Variáveis de pasta------------------------------------#
+
 DEB_DOWNLOADS="$HOME/Downloads/DEB"
+APPIMAGE="$HOME/Apps"
 
+#-------------------------------------Adicionando PPA------------------------------------ #
 
-#------------------------------Adicionando PPA------------------------------ #
 sudo apt-add-repository "$PPA_QBITORRENT"
 
+#-------------------------------Atualizando o repositório---------------------------------#
 
+sudo apt update -y
 
-#deb
+#--------------------------------------Download DEB-------------------------------------- #
+
+mkdir "$DEB"
+wget -c "$URL_CHROME"
+
+#-----------------------------------Download App Image----------------------------------- #
+
+mkdir "$APPIMAGE"
+
+wget -c "$URL_EMAGE" -P "$Apps"
+
+#-----------------------------------------DEB-------------------------------------------- #
 apt-get install qbittorrent -y
 
-#Flatpak
+#---------------------------------------Flatpak------------------------------------------ #
+
 flatpak install flathub org.gimp.GIMP
