722b5ba2bf1f9b5c21862cccb97e5c73048ee442
odiegoduarte <dduartesouza17@gmail.com>
2020-07-07 19:46:43 -0300

    update

--- README.md
+++ README.md
@@ -1,1 +1,20 @@
-README
+<br>
+
+<p align="center">
+  <img width="922" height="257" src="images/postinstall.png">
+</p>
+
+<br>
+
+Esse shellscript foi criado para automatizar a instalação de programas na minha distro linux, nesse repositório você irá encontrar 3 arquivos **postinstall_desktop.sh**, **postinstall_notebook.sh** e **postintall_template.sh** sendo o desktop e notebook criado para meu usso pessoal com uma lista de programas que pode ser bem diferente da que você utiliza.
+
+<br>
+
+postinstall_ | sobre
+:------ | :------ 
+postintall_template.sh | Template base para você criar seu próprio shellscript automatizado.
+postintall_notebook.sh | Shellscript criado para usso pessoal
+postintall_desktop.sh | Shellscript criado para usso pessoal 
+
+<br>
+<br>
--- postintall_template.sh
+++ postintall_template.sh
@@ -1,40 +1,66 @@
 #!/bin/bash
 
-#Criado por Diego Duarte 2020
-#Template de instalação automatica em Shell Script.
+# Criado por Diego Duarte 2020
+# Template de instalação automatica em Shell Script.
+# Esse template é apenas um modelo de como eu estruturei o meu sh e adicionei alguns exemplos.
+# Sinta-se livre para modificar a forma que quizer
 
-#Variáveis PPA
+
+#Variáveis PPA --------------------------------------------------------------------------------------------#
+
 PPA_QBITORRENT="ppa:qbittorrent-team/qbittorrent-stable"
+#|-----------|  |--------------------------------------| 
+#      |                         |
+#      |                         |------ URL do seu PPA
+#      | 
+#      |------ Nome da váriavel que você vai usar no passo Adicionando ppa
+#
 
-#Variáveis de .deb
+#Variáveis.deb --------------------------------------------------------------------------------------------#
 URL_CHROME="https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb"
 
-#Variáveis AppImage
+
+#Variáveis AppImage ---------------------------------------------------------------------------------------#
 URL_EMAGE"https://github.com/douglasjunior/emage/releases/download/1.1.0/emage-1.1.0-x86_64.AppImage"
 
-#Variáveis das pastas
+
+#Variáveis de pasta ---------------------------------------------------------------------------------------#
 DEB="$HOME/Downloads/DEB"
 APPIMAGE="$HOME/Apps"
 
-#Adicionando PPA no sistema
+
+#Adicionando PPA ------------------------------------------------------------------------------------------#
 sudo apt-add-repository "$PPA_QBITORRENT" --sk
 
-#Atualizando o repositório
+#Atualizando o repositório --------------------------------------------------------------------------------#
+
 sudo apt update -y
 
-#Download dos pacotes .deb
-mkdir "$DEB"       
-wget -c "$URL_CHROME"
+#Download DEB ---------------------------------------------------------------------------------------------#
 
-#Instala todos os pacotes .deb da pasta.
+mkdir "$DEB"
+wget -c "$URL_CHROME"     -P "$DEB"
+
+
 sudo dpkg -i $DEB/*.deb
 
-#Download App Image
-mkdir "$APPIMAGE"
-wget -c "$URL_EMAGE" -P "$Apps"
+#Download App Image ---------------------------------------------------------------------------------------#
 
-#Instala os apps via repositório
+mkdir "$APPIMAGE"
+
+wget -c "$URL_EMAGE"       -P "$APPIMAGE"
+
+#Instala os apps via repositório --------------------------------------------------------------------------#
+
+#PPA de terceiros
 apt-get install qbittorrent -y
 
-#Flatpak instala apps do flathub
-flatpak install flathub org.gimp.GIMP
+#PPA
+apt-get install SimpleScreenRecorder -y
+
+
+#Flatpak --------------------------------------------------------------------------------------------------#
+
+flatpak install flathub org.gimp.GIMP -y
+
+echo "Instalação finalizada !"
