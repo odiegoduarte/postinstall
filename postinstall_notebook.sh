f64801950a1c2d4ba942726ce94d2cc10579b819
odiegoduarte <dduartesouza17@gmail.com>
2020-07-08 22:50:02 -0300

    update

--- postinstall_notebook.sh
+++ postinstall_notebook.sh
@@ -41,10 +41,17 @@ URL_SHOWPLAYER="https://github.com/FrancescoCeruti/linux-show-player/releases/do
 URL_REAPER="https://dlcf.reaper.fm/6.x/reaper612c_linux_x86_64.tar.xz"
 URL_VSXU="https://github.com/vovoid/vsxu/releases/download/v0.6.3/VSXu-0.6.3.glibc2.17.AppImage"
 
+#Variáveis Personalização ---------------------------------------------------------------------------------#
+
+URL_QOGIR="https://dllb2.pling.com/api/files/download/j/eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpZCI6IjE1OTIzMDkxNzkiLCJ1IjpudWxsLCJsdCI6ImRvd25sb2FkIiwicyI6Ijg3OWJiOWViYTY4ZWM5ZjlhMTY3ZWY5ZjMzMjAxNGRhYWE4MGFmZDJhNWVhMzYyMzE4ODZiZjYwOTRkYWM4ZWI5ZDBjMTI3MTkzMzk4ZmNiODhjMTRjZGEyOTcwMDJlOWE2YzI4NmEwNjc3MmVlYzk5ZjFhNjMyMzM5OTY5YzZkIiwidCI6MTU5NDI2MjIxMiwic3RmcCI6IjRmOWEyMWE3ZjQ2YmRiZGQ1NWI1NGRjY2Q2ZDQ1MmU1Iiwic3RpcCI6IjE4Ni4yMjUuMTg2LjcyIn0.gD0YIYkIEUOOpWog1jJZQ_uBNG8ubSROfPVyEdOMkxA/Qogir-dark.tar.xz"
+URL_PAPIRUS="https://dllb2.pling.com/api/files/download/j/eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpZCI6IjE1OTM3NDczOTEiLCJ1IjpudWxsLCJsdCI6ImRvd25sb2FkIiwicyI6IjJhNzlkNjBkZTQ1ZTk1M2UyMTM2MGUxZWFmNjA2N2ZiNTdmMjdjMjVlM2EwYjE0NjU2ZWNmYjBjOGFmYjQ0ZTMyZWNiZjJkZmExNzJjMzBlNDRjNDQ0MDU5YTk2NjhiMTY5ZjU1Mjk2MjUyNDFjZjIxZGM2OGNkNzE1ZGU3YjEzIiwidCI6MTU5NDI2MjM2OCwic3RmcCI6IjRmOWEyMWE3ZjQ2YmRiZGQ1NWI1NGRjY2Q2ZDQ1MmU1Iiwic3RpcCI6IjE4Ni4yMjUuMTg2LjcyIn0.Q45qkfEfGS_BoB3b-ZlOJpQRKJEF1bEhPS_L_eibuyQ/papirus-icon-theme.tar.xz"
+
 #Variáveis de pasta ---------------------------------------------------------------------------------------#
 
 DEB="$HOME/Downloads/DEB"
 APPIMAGE="$HOME/Apps"
+ICONS="$HOME/.icons"
+THEMES="$HOME/.themes"
 
 echo "Adicionando PPA ao Sistema"
 
@@ -61,6 +68,13 @@ sudo rm /var/cache/apt/archives/lock
 #Atualizando o repositório --------------------------------------------------------------------------------#
 
 sudo apt update -y
+
+echo "Baixando temas e icones"
+
+#Personalização --------------------------------------------------------------------------------------------#
+
+wget -c "$URL_QOGIR"     -P "$THEMES"
+wget -c "$URL_PAPIRUS"   -P "$ICONS"
 
 echo "Baixando e instalando programas .DEB"
 
@@ -92,8 +106,6 @@ wget -c "$URL_VSXU"        -P "$APPIMAGE"
 
 echo "Instalando programas dos PPA's"
 
-
-
 #Instala os apps via repositório --------------------------------------------------------------------------#
 
 #PPA de terceiros
@@ -106,6 +118,7 @@ sudo apt install SimpleScreenRecorder -y
 sudo apt install audacity -y
 sudo apt install spotify-client -y
 sudo apt install steam -y
+sudo apt install mint-meta-codecs -y
 
 echo "Instalando programas em Flatpak"
 
@@ -116,5 +129,24 @@ flatpak install flathub org.gimp.GIMP.Plugin.GMic -y
 flatpak install flathub org.inkscape.Inkscape -y
 flatpak install flathub org.kde.kdenlive -y
 flatpak install flathub net.sourceforge.projectM -y
+
+#Removendo programas --------------------------------------------------------------------------------------#
+
+sudo apt remove transmission -y
+sudo apt remove hexchat -y
+sudo apt remove thunderbird -y
+sudo apt remove drawing -y
+sudo apt remove pix -y
+
+#Atualização e limpeza do sistema -------------------------------------------------------------------------#
+
+sudo snap set system refresh.retain=2
+
+flatpak update
+flatpak uninstall --unused -y
+
+sudo apt update -y
+sudo apt autoremove -y
+sudo apt autoclean -y
 
 echo "Instalação finalizada !"
